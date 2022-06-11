71dd90e2c769747c2e5042f511cd4a0d8fc760be
fhizyelnazareta <fhizyel.nazareta@cinema21.co.id>
2022-06-11 18:56:17 +0700

    change color

--- maven-soal-test2/script.js
+++ maven-soal-test2/script.js
@@ -1,8 +1,15 @@
 function chartView(angka) {
   let n =[];
   for(let i = 1; i <= angka; i++){
-    n.push({x: i, y: Math.random()*100 }) 
+    n.push({x: i, y: Math.random()*100}) 
   }
+
+  const item = n.map(item => {
+    item.y <= 25 ? item.color = "green" : item.y > 25 && item.y <= 50 ? item.color = "yellow" : item.y > 50 && item.y <= 75 ? item.color = "red" : item.y > 75 && item.y <= 100 ? item.color = "blue": 'white'
+    return item;
+  });
+
+  console.log(item)
 
   var chart = new CanvasJS.Chart("chartContainer",
   {
